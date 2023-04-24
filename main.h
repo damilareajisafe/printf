b5ed499ef3b4a1f3e4a282c5e46ab4e43d8470e2
Tabrita <tabritajames@yahoo.com>
2023-04-24 15:37:54 -0700

    header file updated

--- main.h
+++ main.h
@@ -1,6 +1,24 @@
+#include <stdarg.h>
 #ifndef MAIN_H
 #define MAIN_H
+/**
+ * struct specifiers - Struct specifiers
+ *
+ * @sp: the specifier
+ * @f: the function associated with @sp
+ */
+
+typedef struct specifiers
+{
+    const char *sp;
+    int (*f)(va_list);
+} specifier_t;
 
 int _printf(const char *format, ...);
+int print_int(va_list);
+int print_char(va_list);
+int print_string(va_list);
+int (*check_specifier(const char*))(va_list);
+int handle_conversion(const char*, va_list);
 
 #endif
