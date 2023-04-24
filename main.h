694a572d402c1482d0d03d853764069a37a9d74b
damilareajisafe <dammyajisafe@gmail.com>
2023-04-24 15:50:14 -0700

    updated main.h

--- main.h
+++ main.h
@@ -1,6 +1,6 @@
-#include <stdarg.h>
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
 /**
  * struct specifiers - Struct specifiers
  *
@@ -10,15 +10,14 @@
 
 typedef struct specifiers
 {
-    const char *sp;
-    int (*f)(va_list);
+	const char *sp;
+	int (*f)(va_list);
 } specifier_t;
 
 int _printf(const char *format, ...);
 int print_int(va_list);
 int print_char(va_list);
 int print_string(va_list);
-int (*check_specifier(const char*))(va_list);
-int handle_conversion(const char*, va_list);
-
+int (*check_specifier(const char *))(va_list);
+int handle_conversion(const char *, va_list);
 #endif
